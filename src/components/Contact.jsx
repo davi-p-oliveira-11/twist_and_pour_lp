9d7e19562bf7c09e27e9ce3b23033a636a561c12
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-01-12 14:46:39 -0300

    fix: contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,13 +39,13 @@ const Contact = () => {
 		
 		<div>
 		 <h3>Visit Our Bar</h3>
-		 <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+		 <p>742, Orion Ave. #315, San Diego, CA 92101</p>
 		</div>
 		
 		<div>
 		 <h3>Contact Us</h3>
 		 <p>(555) 987-6543</p>
-		 <p>hello@jsmcocktail.com</p>
+		 <p>dpo.dev11@gmail.com</p>
 		</div>
 		
 		<div>
